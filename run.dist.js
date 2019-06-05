b24fdfa2dcfe5713199191ce2124b6dec5f54f88
weber <528066535@qq.com>
2019-06-05 14:38:29 +0800

    提交

--- run.dist.js
+++ run.dist.js
@@ -1,8 +1,5 @@
 const koa = require('koa');
 const path = require('path');
-const config = require('./build/webpack.prod.js');
-const webpack = require('webpack');
-const middleware = require('webpack-koa2-middleware');
 const CONFIG = require('./CONFIG');
 const proxy = require('koa-proxies');
 const koaStatic = require('koa-static');
@@ -28,14 +25,6 @@ koaEjs(app, {
     cache: false,
     debug: true
 });
-
-// const compiler = webpack(config);
-// app.use(middleware(compiler, {
-//     index: 'index.html',
-//     stats: {
-//         colors: true
-//     }
-// }));
 
 app.use(proxy('/api', {
     target: CONFIG.API_URL,
